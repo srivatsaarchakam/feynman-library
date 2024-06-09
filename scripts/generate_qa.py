0a75490767a22b14cdbf11b233584c6d0ce24bfa
Srivatsa Krishnamurthy <srivatsa.s.krishnamurthy@gmail.com>
2024-06-08 18:51:43 -0700

    mistral-7b-data-format

--- scripts/generate_qa.py
+++ scripts/generate_qa.py
@@ -1,40 +1,37 @@
 import json
 import spacy
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-# load NLP Model and the question generation pipline
-nlp = spacy.load("en_core_web_sm")
-q_generator = pipeline("text2text-generation", model="valhalla/t5-small-qg-prepend")
+# load spacy & mistral-7b-instruct
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+mistral_generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
-def generate_convo(text):
-    doc = nlp(text)
-    messages = []
-    for sent in doc.sents:
-        generated = q_generator("generate question: " + sent.text, max_length=512)
-        question  = generated[0]['generated_text']
-        for item in generated:
-            messages.append({"role": "user", "content": question})
-            messages.append({"role": "assistant", "content": sent.text})
+# q/a pairs generation & propmpting
+def generate_qa_pairs(text):
+    prompt = f"Input Text: {text}\n"\
+             "Context:Answering Physics Questions Based on Richard Feynman's Lectures on Physics\n" \
+             "Given the text excerpt provided, create five standalone question and answer pairs where each question contains a question about physics or about the life of Richard Feynman. The questions should explore the underlying physics concepts, Feynman's interpretations as presented in the text, or information about his life. Answers should directly use, verbatim, phrases or sentences from the text to ensure accuracy and maintain the original context of Feynman's teachings. Each question and answer pair should be crafted as if they are independent, without requiring context from other pairs, and should vary in style to cover different aspects or perspectives of the content. Do not number the questions."
+    
+    generated = mistral_generator(prompt, max_new_tokens=250)[0]['generated_text']
+    return generated
 
-    return {"messages":messages}
-
+# data processing
 def process_data(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
         all_conversations = []
 
         for entry in data:
-            text_entries = [item['data'] for item in entry['output'] if item['type'] == 'text'][:10]
+            text_entries = [item['data'] for item in entry['output'] if item['type'] == 'text'][:1]
             text = " ".join(text_entries)
-            conversation = generate_convo(text)
-            all_conversations.append(conversation)
+            generated_qa_pairs = generate_qa_pairs(text)
+            all_conversations.append({"messages": generated_qa_pairs})
 
     return all_conversations
 
-# current data path
-file_path = file_path = '/Users/srivatsakrishnamurthy/Documents/dev/FeynmanLibrary/data/feynman_lectures_data.json'
-conversations = process_data(file_path)
+conversations = process_data("/Users/srivatsakrishnamurthy/Documents/dev/FeynmanLibrary/data/feynman_lectures_data.json")
 
-# save new data
-with open('/data/formatted_data.json', 'w') as f:
-    json.dump(conversations, f, indent=4)
+save_path = "/Users/srivatsakrishnamurthy/Documents/dev/FeynmanLibrary/data/qa_pairs.jsonl"
+with open(save_path, 'w') as f:
+    json.dump(conversations,f, indent=4)
